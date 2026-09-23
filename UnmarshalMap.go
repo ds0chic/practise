131e78cd4537b0845cbd711aaed011aff81a20cb
ds0chic <dsochic159357@henu.edu.cn>
2026-09-23 15:11:19 +0800

    feat: add UnmarshalMap demo and sync readme

---
+++ UnmarshalMap.go
@@ -0,0 +1,19 @@
+package practise
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func UnmarshalMapDemo() {
+	userdata := []byte(`[{"name":"Tom","age":22},{"name":"Bob","age":25}]`)
+	var user []map[string]any
+	err := json.Unmarshal(userdata, &user)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(user)
+	fmt.Println(user[1]["name"])
+}
